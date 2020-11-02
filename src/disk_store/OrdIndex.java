a91eaba52dcb792ace8c2866667c5f5b41a515a7
casscabrera <cass.cabrera99@gmail.com>
2020-11-01 21:24:11 -0800

    completed functions in OrdIndex

--- src/disk_store/OrdIndex.java
+++ src/disk_store/OrdIndex.java
@@ -41,22 +41,91 @@ public class OrdIndex implements DBIndex {
 		// binary search of entries arraylist
 		// return list of block numbers (no duplicates). 
 		// if key not found, return empty list
-		throw new UnsupportedOperationException();
+		int first = 0;
+		int last = entries.size()-1;
+		List<Integer> blockNums = new ArrayList<>();
+
+		while(first <= last) {
+			int middle = (first + last) / 2;
+			if(entries.get(middle).key == key) {
+				for(BlockCount t : entries.get(middle).blocks) {
+					if(!blockNums.contains(t.blockNo)) {
+						blockNums.add(t.blockNo);
+					}
+				}
+				return blockNums;
+			} else if(entries.get(middle).key > key) {
+				last = middle - 1;
+			} else {
+				first = middle + 1;
+			}
+		}
+		return blockNums;
 	}
 	
 	@Override
 	public void insert(int key, int blockNum) {
-		throw new UnsupportedOperationException();
+		boolean foundKey = false;
+		int position = -1;
+		for(int i = 0; i < entries.size(); i++) {
+			if(entries.get(i).key == key) {
+				foundKey = true;
+				position = i;
+				for (BlockCount b : entries.get(i).blocks) {
+					if (b.blockNo == blockNum) {
+						b.count++;
+						return;
+					}
+				}
+			}
+		}
+
+		BlockCount b = new BlockCount();
+		b.count = 1;
+		b.blockNo = blockNum;
+
+		if(!foundKey) {
+			Entry e = new Entry();
+			e.key = key;
+			e.blocks = new ArrayList<>();
+			e.blocks.add(b);
+			entries.add(e);
+			size++;
+		} else {
+			entries.get(position).blocks.add(b);
+			size++;
+		}
+
+
 	}
 
 	@Override
 	public void delete(int key, int blockNum) {
-		// lookup key 
-		//  if key not found, should not occur.  Ignore it.
-		//  decrement count for blockNum.
-		//  if count is now 0, remove the blockNum.
-		//  if there are no block number for this key, remove the key entry.
-		throw new UnsupportedOperationException();
+		List<Integer> result = lookup(key);
+		// lookup key
+		if(result.isEmpty()) {
+			return;
+		}
+
+		for(Entry e : entries) {
+			if(e.key == key) {
+				if(e.blocks.isEmpty()) {
+					entries.remove(e);
+					size--;
+					return;
+				}
+				for(BlockCount b : e.blocks) {
+					if(b.blockNo == blockNum) {
+						b.count--;
+						if(b.count == 0) {
+							e.blocks.remove(b);
+							size--;
+						}
+						return;
+					}
+				}
+			}
+		}
 	}
 	
 	/**
